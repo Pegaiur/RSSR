4aefc5fdea2296e7d3b65944373261fdaec3d164
Pegasus <pegasus@yunio.com>
2015-10-24 23:43:56 +0800

    Initial Commit

---
+++ RSSR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSSR
+//
+//  Created by 李锦心 on 15/10/24.
+//  Copyright © 2015年 李锦心. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
