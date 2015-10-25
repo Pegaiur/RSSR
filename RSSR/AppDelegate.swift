db50d6818c859e0e358104439a8291b632d90d45
Pegasus <pegasus@yunio.com>
2015-10-25 16:53:14 +0800

    start

--- RSSR/AppDelegate.swift
+++ RSSR/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.makeKeyAndVisible()
+        let c = ViewController()
+        let nv = UINavigationController(rootViewController: c)
+        self.window?.rootViewController = nv
         return true
     }
 
--- RSSR/ViewController.swift
+++ RSSR/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MWFeedParser
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,MWFeedParserDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://v2ex.com/feed/tab/tech.xml")
+        let feedParser = MWFeedParser(feedURL: url)
+        feedParser.delegate = self
+        feedParser.feedParseType = ParseTypeFull
+        feedParser.connectionType = ConnectionTypeSynchronously
+        feedParser.parse()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+//  MARK:MWFeedParserDelegate
+    
+    func feedParser(parser: MWFeedParser!, didParseFeedInfo info: MWFeedInfo!) {
+        print(info)
+    }
+    
+    func feedParser(parser: MWFeedParser!, didParseFeedItem item: MWFeedItem!) {
+        print(item)
+    }
+    
+    
 }
 
