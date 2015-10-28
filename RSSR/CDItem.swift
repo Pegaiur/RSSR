2714a67525f4accac764bf9e12645d06185cbed0
Pegasus <pegasus@yunio.com>
2015-10-28 17:24:28 +0800

    coredata fix

--- RSSR/CDItem.swift
+++ RSSR/CDItem.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-
+@objc(CDItem)
 class CDItem: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
--- RSSR/CDSource.swift
+++ RSSR/CDSource.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-
+@objc(CDSource)
 class CDSource: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
